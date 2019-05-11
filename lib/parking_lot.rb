149ebd22a17d0a42e5f679b7bd9e910726553a71
Karun <karunk@live.com>
2019-05-12 00:25:41 +0530

    Define functions for parking_lot

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -1,18 +1,30 @@
 class ParkingLot
 
-  include ParkingLotCommandProcessor
-
   def initialize
   end
 
-  def park
+  def park(car)
+    #will return a ticket
+  end
+
+  def unpark(ticket)
   end
 
   def status
   end
 
-  def greeting
-    "Ruby Example"
+  def get_slot_of_reg_no(registration_no)
+  end
+
+  def get_reg_nos_for_colour(colour)
+  end
+
+  def get_slot_nos_for_colour(colour)
+  end
+
+private
+  
+  def create_slot
   end
 
 end
