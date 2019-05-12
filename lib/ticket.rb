4aa757b41e8559ef55f82af1f3e1c95ae729a788
Karun <karunk@live.com>
2019-05-12 22:59:13 +0530

    Add hash map functionality for registration number to slot number mapping

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -4,6 +4,7 @@ class Ticket
 
   @@active_ticket_pool = {}
   @@tickets_car_colour_map = {}
+  @@ticket_car_map = {}
 
   def self.issue_ticket!(car)
     slot = Slot.new
@@ -28,6 +29,18 @@ class Ticket
     return @@active_ticket_pool.length
   end
 
+  def self.tickets_currently_issued
+    self.active_ticket_pool_size
+  end
+
+  def self.valid_ticket?(ticket_id)
+    return @@active_ticket_pool.key?(ticket_id)
+  end
+
+  def self.ticket_issued?(car)
+    @@ticket_car_map.key?(car.registration_number)
+  end
+
   def parked_car
     @parked_slot.car
   end
@@ -39,12 +52,18 @@ class Ticket
   def forfeit_ticket!
     @@active_ticket_pool.delete(self.ticket_id)
     @@tickets_car_colour_map[parked_car.colour].delete(self.ticket_id)
+    @@ticket_car_map.delete(parked_car.registration_number)
     @parked_slot.unpark!
     @parked_slot = nil
   end
 
   def validity?
     !@parked_slot.nil?
+  end
+
+  def self.get_issued_ticket_for_car!(car_registration_number)
+    raise StandardError.new('Not found') unless @@ticket_car_map.key?(car_registration_number)
+    return @@ticket_car_map[car_registration_number]
   end
 
   private
@@ -54,12 +73,15 @@ class Ticket
     @@active_ticket_pool[self.ticket_id] = self
     @@tickets_car_colour_map[parked_car.colour]||= [] 
     @@tickets_car_colour_map[parked_car.colour]<<self.ticket_id
+    @@ticket_car_map[parked_slot.car.registration_number] = self.ticket_id
   end
 
   def self.reset_active_ticket_pool
     @@active_ticket_pool = {}
     @@tickets_car_colour_map = {}
+    @@ticket_car_map = {}
   end
+
 end
 
 
