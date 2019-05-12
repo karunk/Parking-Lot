331fd1122e033bbae1ab0a7dc79e4579dc58b106
Karun <karunk@live.com>
2019-05-13 01:19:26 +0530

    Add Commands

---
+++ lib/commands.rb
@@ -0,0 +1,23 @@
+module Commands
+
+  PARK = 'park'
+  CREATE_PARKING_LOT = 'create_parking_lot'
+  LEAVE = 'leave'
+  STATUS = 'status'
+  REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR = 'registration_numbers_for_cars_with_colour'
+  SLOT_NUMBERS_FOR_CARS_WITH_COLOUR = 'slot_numbers_for_cars_with_colour'
+  SLOT_NUMBER_FOR_REGISTRATION_NUMBER = 'slot_number_for_registration_number'
+
+  VALID_PARKING_LOT_COMMANDS = [
+    PARK,
+    CREATE_PARKING_LOT,
+    LEAVE,
+    STATUS,
+    REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR,
+    SLOT_NUMBERS_FOR_CARS_WITH_COLOUR,
+    SLOT_NUMBER_FOR_REGISTRATION_NUMBER
+  ]
+
+  
+
+end
