6386ca8c2ecc221efe882e4b3c77270a49c26677
Karun <karunk@live.com>
2019-05-12 19:19:11 +0530

    Add Ticket and Ticket spec

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,10 +1,66 @@
 class Ticket
 
-  attr_reader :parked_car, :parking_lot_slot
+  attr_accessor :parked_slot
 
-  def initialize(car, slot)
-    @parked_car = car
-    @parking_lot_slot = slot
+  @@active_ticket_pool = {}
+  @@tickets_car_colour_map = {}
+
+  def self.issue_ticket!(car)
+    slot = Slot.new
+    slot.park!(car)
+    ticket = Ticket.new(slot)
+    return ticket
   end
 
+  def self.active_tickets_for_car_colour(colour)
+    return @@tickets_car_colour_map[colour].map{ |ticket_id| fetch_ticket!(ticket_id) }
+  end
+
+  def self.fetch_ticket!(ticket_id)
+    return @@active_ticket_pool[ticket_id]
+  end
+
+  def self.active_ticket_pool
+    return @@active_ticket_pool
+  end
+
+  def self.active_ticket_pool_size
+    return @@active_ticket_pool.length
+  end
+
+  def parked_car
+    @parked_slot.car
+  end
+
+  def ticket_id
+    self.validity? ? @parked_slot.slot_number : nil
+  end
+
+  def forfeit_ticket!
+    @@active_ticket_pool.delete(self.ticket_id)
+    @@tickets_car_colour_map[parked_car.colour].delete(self.ticket_id)
+    @parked_slot.unpark!
+    @parked_slot = nil
+  end
+
+  def validity?
+    !@parked_slot.nil?
+  end
+
+  private
+
+  def initialize(parked_slot)
+    @parked_slot = parked_slot
+    @@active_ticket_pool[self.ticket_id] = self
+    @@tickets_car_colour_map[parked_car.colour]||= [] 
+    @@tickets_car_colour_map[parked_car.colour]<<self.ticket_id
+  end
+
+  def self.reset_active_ticket_pool
+    @@active_ticket_pool = {}
+    @@tickets_car_colour_map = {}
+  end
 end
+
+
+
--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -2,25 +2,140 @@ require 'ticket'
 require 'slot'
 require 'car'
 require 'faker'
-
+require 'byebug'
 RSpec.describe Ticket do
 
   def unique_registration_number
     return Faker::Name.initials(2).to_s+'-'+Faker::Number.between(0, 99).to_s+'-'+Faker::Name.initials(2).to_s+'-'+Faker::Number.number(4).to_s 
   end
 
-  let(:colour) { Faker::Color.color_name.capitalize }
-  let(:registration_number) { unique_registration_number }
-  let(:car) { Car.new(registration_number, colour) }
-  let(:slot) { Slot.new }
-  subject { Ticket.new(car, slot) }
+  def pick_colour
+    Faker::Color.color_name.capitalize
+  end
 
-  it "to identify a parked car and a parking lot slot" do
-    expect(subject.parked_car).to be_instance_of(Car)
-    expect(subject.parked_car).to equal(car)
+  let(:car) { Car.new(unique_registration_number, pick_colour) }
 
-    expect(subject.parking_lot_slot).to be_instance_of(Slot)
-    expect(subject.parking_lot_slot).to equal(slot)
+  describe "Issuing a Ticket" do
+    before(:each) do 
+      Ticket.reset_active_ticket_pool
+    end
+
+    it "A ticket is issued to a car" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket).to be
+      expect(ticket).to be_an_instance_of(Ticket)
+    end
+
+    it "Active Ticket Pool (ATP) size increases when a ticket is issued" do
+      expect(Ticket.active_ticket_pool).to be_an_instance_of(Hash)
+      expect(Ticket.active_ticket_pool_size).to eql(0)
+      ticket = Ticket.issue_ticket!(car)
+      expect(Ticket.active_ticket_pool_size).to eql(1)
+    end
+
+    it "Ticket being issued is maintained in Active Ticket Pool (ATP)" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(Ticket.active_ticket_pool).to have_key(ticket.ticket_id)
+      expect(Ticket.active_ticket_pool[ticket.ticket_id]).to equal(ticket)
+    end
+
+    it "A slot is occupied when a ticket is issued" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket.parked_slot).to be_an_instance_of(Slot)
+      expect(ticket.parked_slot.vacant?).to be false
+      expect(ticket.parked_slot.slot_number).to be
+    end
+
+  end
+
+  describe "Forfeiting a Ticket" do
+    before(:each) do 
+      Ticket.reset_active_ticket_pool
+    end
+
+    it "Ticket being forfeited is removed from Active Ticket Pool (ATP)" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(Ticket.active_ticket_pool).to have_key(ticket.ticket_id)
+      ticket.forfeit_ticket!
+      expect(Ticket.active_ticket_pool).to_not have_key(ticket.ticket_id)
+    end
+
+    it "Active Ticket Pool (ATP) size decreases when a ticket is forfeited" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(Ticket.active_ticket_pool_size).to eql(1)
+      ticket.forfeit_ticket!
+      expect(Ticket.active_ticket_pool_size).to eql(0)
+    end
+
+    it "A slot is freed and the car is unparked when a ticket is forfeited" do
+      slot_number_issued = Slot.peek_next_slot_number
+      ticket = Ticket.issue_ticket!(car)
+      expect(Slot.peek_next_slot_number).to_not eql(ticket.parked_slot.slot_number)
+      expect(slot_number_issued).to eql(ticket.parked_slot.slot_number)
+      ticket.forfeit_ticket!
+      expect(Slot.peek_next_slot_number).to eql(slot_number_issued)
+      expect(ticket.parked_slot).to be nil
+    end
+
+    it "Invalidates the ticket" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket.validity?).to be (true)
+      ticket.forfeit_ticket!
+      expect(ticket.validity?).to be (false)
+    end
+
+  end
+
+  describe "Ticket Id" do
+
+    it "Ticket Id is generated when a ticket is issued" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket.ticket_id).to be 
+    end
+
+    it "Ticket Id is equal to the slot number where car is parked" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket.ticket_id).to eql(ticket.parked_slot.slot_number)
+      expect(ticket.ticket_id).to eql(Slot.peek_next_slot_number-1)
+    end
+
+    it "Expect ticket Id to be nil when a ticket is forfeited" do
+      ticket = Ticket.issue_ticket!(car)
+      ticket.forfeit_ticket!
+      expect(ticket.ticket_id).to be nil
+    end 
+
+  end
+
+  describe "Fetching Tickets" do
+    before(:each) do 
+      Ticket.reset_active_ticket_pool
+    end
+
+    it "A ticket can be fetched by a ticket id" do
+      ticket = Ticket.issue_ticket!(car)
+      expect(ticket).to be == (Ticket.fetch_ticket!(ticket.ticket_id))
+    end
+
+    it "All tickets assigned to cars of a particular colour can be fetched" do
+      color_map = {}
+      (1..1000).each do |i|
+        car = Car.new(unique_registration_number, pick_colour)
+        color_map[car.colour]||=[]
+        color_map[car.colour] << Ticket.issue_ticket!(car)
+      end
+      color_map.each do |key, value|
+        expected_list_of_car_registration_nos = color_map[key].map{ |ticket| ticket.parked_slot.car.registration_number }
+        expected_list_of_slot_numbers = color_map[key].map{ |ticket| ticket.parked_slot.slot_number }
+
+        actual_list_of_car_registration_nos = Ticket.active_tickets_for_car_colour(key).map{ |ticket| ticket.parked_slot.car.registration_number }
+        actual_list_of_slot_numbers = Ticket.active_tickets_for_car_colour(key).map{ |ticket| ticket.parked_slot.slot_number }
+
+        expect(actual_list_of_car_registration_nos).to be == expected_list_of_car_registration_nos
+        expect(actual_list_of_slot_numbers).to be == expected_list_of_slot_numbers
+      end
+    end
+
   end
 
 end
