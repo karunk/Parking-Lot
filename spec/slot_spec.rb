2fbc7e310e431cd580f256959af5fdb521c2c917
Karun <karunk@live.com>
2019-05-12 16:02:50 +0530

    Add tests for Slot number assignment

--- spec/slot_spec.rb
+++ spec/slot_spec.rb
@@ -1,13 +1,59 @@
 require 'slot'
 require 'byebug'
+require 'faker'
 RSpec.describe Slot do
 
-  describe 'slot number testing' do
-    
+  describe 'slot number assignment' do
+    before(:each) do 
+      Slot.reset_slot_numbers_pool
+    end
+
     it 'each slot is issued a slot number' do
       slot = Slot.new
-      expect(slot.slot_number).to be
+      expect(slot.slot_number).to eql(1)
     end
+
+    it 'each slot is issued a slot number in ascending order' do
+      (1..Faker::Number.between(1, 500)).each do |i|
+        slot = Slot.new
+        expect(slot.slot_number).to eql(i)
+      end
+    end
+
+    it 'slot number pool is replenished automatically after exhaustion' do
+      (1..100).each do |i|
+        slot = Slot.new
+      end
+      expect(Slot.peek_next_slot_number).to be_nil
+      (101..1000).each do |i|
+        slot = Slot.new
+      end
+      expect(Slot.peek_next_slot_number).to be_nil
+      slot = Slot.new
+      expect(Slot.peek_next_slot_number).to eql(1002)
+    end
+
+    it 'lowest slot number is assigned at all times' do
+      assigned_slots = [nil]
+      (1..150).each do |i|
+        slot = Slot.new
+        assigned_slots<<slot
+      end
+      assigned_slots[5].unpark
+      expect(Slot.peek_next_slot_number).to eql(5)
+      slot = Slot.new
+      expect(slot.slot_number).to eql(5)
+      assigned_slots[77].unpark
+      assigned_slots[98].unpark
+      assigned_slots[34].unpark
+      next_three_slot_numbers = [34, 77, 98]
+      next_three_slot_numbers.each do |slot_number|
+        slot = Slot.new
+        expect(slot.slot_number).to eql(slot_number)
+      end
+      expect(Slot.peek_next_slot_number).to eql(151)
+    end
+
 
   end
 
