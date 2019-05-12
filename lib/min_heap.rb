56cc601d7b70b151a7157587a67c9037e3f3557b
Karun <karunk@live.com>
2019-05-12 15:20:54 +0530

    Add Min Heap and min_heap_spec

---
+++ lib/min_heap.rb
@@ -0,0 +1,96 @@
+class MinHeap
+
+  def initialize
+    @elements = [nil] # initialize an empty array with nil at the 0 index (to make math easier)
+    @element_position_map = Hash.new
+  end
+
+  def <<(element)
+    @elements << element
+    @element_position_map[element] = @elements.size - 1
+    sift_up(@elements.size - 1)
+  end
+
+  def count
+    return @elements.size - 1
+  end
+
+  def elements
+    return @elements.drop(1)
+  end
+
+  def peek_min
+    return @elements[1]
+  end
+
+  def extract_min
+    exchange(1, @elements.size - 1)
+    min_element = @elements.pop
+    sift_down(1)
+    return min_element
+  end
+
+  def contains_element(element)
+    return @element_position_map.has_key?(element)
+  end
+
+  def delete_element(element)
+    element_position = @element_position_map[element]
+    unless element_position.nil?
+      exchange(element_position, @elements.size - 1)
+      element_to_remove = @elements.pop
+      @element_position_map.delete(element_to_remove)
+      sift_down(element_position)
+      return element_to_remove
+    end
+  end
+
+  def print_heap
+    puts 'printing min heap'
+    @elements.each do |element|
+      if element.nil?
+        puts 'nil'
+      else
+        puts element
+      end
+    end
+  end
+
+  private 
+
+
+  def sift_up(index)
+    parent_index = (index / 2)
+    return if index <= 1
+    return if @elements[index] >= @elements[parent_index]
+    exchange(index, parent_index)
+    sift_up(parent_index)
+  end
+
+  def sift_down(index)
+    child_index = (index * 2)
+    return if child_index > @elements.size - 1
+    not_the_last_element = child_index < @elements.size - 1
+    left_child = @elements[child_index]
+    right_child = @elements[child_index + 1]
+    if not_the_last_element && right_child < left_child
+      child_index += 1
+    end
+    return if @elements[index] <= @elements[child_index]
+    exchange(index, child_index)
+    sift_down(child_index)
+  end
+
+  def exchange(source_index, target_index)
+    tmp_source = @elements[source_index]
+    tmp_target = @elements[target_index]
+    source_element_position = @element_position_map[tmp_source]
+    target_element_position = @element_position_map[tmp_target]
+    @elements[source_index] = tmp_target
+    @elements[target_index] = tmp_source
+    @element_position_map[tmp_source] = target_element_position
+    @element_position_map[tmp_target] = source_element_position
+  end
+
+end
+
---
+++ spec/min_heap_spec.rb
@@ -0,0 +1,144 @@
+require 'min_heap'
+
+RSpec.describe MinHeap, type: :class do
+
+  describe 'initialization tests' do
+
+    it 'initializes with a null element at the first position' do
+      min_heap = MinHeap.new
+      expect(min_heap.count).to eq(0)
+    end
+
+    it 'peek min is nil with no elements' do
+      min_heap = MinHeap.new
+      expect(min_heap.peek_min).to eq(nil)
+    end
+
+  end
+
+  describe 'min heap ordering on insert' do
+
+    it 'preserves ordering of one element' do
+      min_heap = MinHeap.new
+      min_heap << 1
+      expect(min_heap.count).to eq(1)
+      expect(min_heap.peek_min).to eq(1)
+    end
+
+    it 'preserves ordering of two elements in order' do
+      min_heap = MinHeap.new
+      min_heap << 1
+      min_heap << 2
+      expect(min_heap.count).to eq(2)
+      expect(min_heap.peek_min).to eq(1)
+    end
+
+    it 'preserves ordering of two elements reverse' do
+      min_heap = MinHeap.new
+      min_heap << 2
+      min_heap << 1
+      expect(min_heap.count).to eq(2)
+      expect(min_heap.peek_min).to eq(1)
+    end
+
+    it 'preserves ordering of three elements out of order' do
+      min_heap = MinHeap.new
+      min_heap << 3
+      min_heap << 1
+      min_heap << 2
+      expect(min_heap.count).to eq(3)
+      expect(min_heap.peek_min).to eq(1)
+    end
+
+  end
+
+  describe 'extract min testing' do
+
+    it 'insert in asc - preserves ordering after extract min' do
+      min_heap = MinHeap.new
+      for i in 1..10
+        min_heap << i
+      end
+      expect(min_heap.count).to eq(10)
+      expect(min_heap.peek_min).to eq(1)
+
+      min_element = min_heap.extract_min
+      expect(min_element).to eq(1)
+      expect(min_heap.count).to eq(9)
+    end
+
+    it 'insert in desc - preserves ordering after extract min' do
+      min_heap = MinHeap.new
+      for i in 1..10
+        min_heap << 11-i
+      end
+      expect(min_heap.count).to eq(10)
+      expect(min_heap.peek_min).to eq(1)
+
+      min_element = min_heap.extract_min
+      expect(min_element).to eq(1)
+      expect(min_heap.count).to eq(9)
+    end
+
+    it 'insert random - preserves ordering after extract min' do
+      min_heap = MinHeap.new
+
+      test_elements = [7,5,8,12,6]
+
+      test_elements.each do |element|
+        min_heap << element
+      end
+
+      expect(min_heap.count).to eq(5)
+      expect(min_heap.peek_min).to eq(5)
+
+      elements_after_insertion = [5,6,8,12,7]
+
+      min_heap.elements.each_with_index do |element, index|
+        expect(element).to eq(elements_after_insertion[index])
+      end
+
+      min_element = min_heap.extract_min
+      expect(min_element).to eq(5)
+      expect(min_heap.count).to eq(4)
+
+      elements_after_remove_min = [6,7,8,12]
+
+      min_heap.elements.each_with_index do |element, index|
+        expect(element).to eq(elements_after_remove_min[index])
+      end
+    end
+
+  end
+
+  describe 'delete element testing' do
+
+    it 'deletes an element and preserves ordering' do
+      min_heap = MinHeap.new
+
+      test_elements = [7,5,8,12,6]
+
+      test_elements.each do |element|
+        min_heap << element
+      end
+
+      expect(min_heap.count).to eq(5)
+      expect(min_heap.peek_min).to eq(5)
+
+      element_to_delete = 12
+      deleted_element = min_heap.delete_element(element_to_delete)
+
+      expect(deleted_element).to eq(12)
+      expect(min_heap.count).to eq(4)
+
+      elements_after_deletion = [5,6,8,7] # remove '12'
+
+      min_heap.elements.each_with_index do |element, index|
+        expect(element).to eq(elements_after_deletion[index])
+      end
+    end
+
+
+  end
+
+end
