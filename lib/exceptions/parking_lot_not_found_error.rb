7367b19a844afdbba66b437aa89308891a75a861
Karun <karunk@live.com>
2019-05-13 08:25:30 +0530

    Text formatting

--- lib/exceptions/parking_lot_not_found_error.rb
+++ lib/exceptions/parking_lot_not_found_error.rb
@@ -1,6 +1,6 @@
 class ParkingLotNotFoundError < StandardError
 
-  def initialize(msg='Parking Lot has not been created. Create it first.')
+  def initialize(msg='Parking Lot has not been created. Create it first')
     super
   end
 
